fe1b9cc763f286e257dc767aad03777e1841209c
dorraturki <56479877+dorraturki@users.noreply.github.com>
2019-12-13 17:47:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAecB-vSSD3HFKnwX-uNXrb4Ums6qcygeY",
+    authDomain: "comita-8d2be.firebaseapp.com",
+    databaseURL: "https://comita-8d2be.firebaseio.com",
+    projectId: "comita-8d2be",
+    storageBucket: "comita-8d2be.appspot.com",
+    messagingSenderId: "610234716805",
+    appId: "1:610234716805:web:54bf40055d960fd0804fec",
+    measurementId: "G-1R28KC1SRX"
+  };
+  firebase.initializeApp(firebaseConfig);
+// Firebase
